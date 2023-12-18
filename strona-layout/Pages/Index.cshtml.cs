e62a489822cfeddc22fa5ecd05229c7e81f588d2
K4P1_090 <k4p1.090@gmail.com>
2023-12-18 19:07:55 +0100

    asdas

--- strona-layout/Pages/Index.cshtml.cs
+++ strona-layout/Pages/Index.cshtml.cs
@@ -5,6 +5,23 @@ namespace MyApp.Namespace
 {
     public class IndexModel : PageModel
     {
+        [BindProperty]
+        public List<String> Carols { get; set; }
+        public IndexModel()
+        {
+            Carols= new List<string>{
+                "Cicha noc",
+                "Gdy się Chrystus rodzi",
+                "Bóg się rodzi",
+                "Lulajże, Jezuniu",
+                "Przybieżeli do Betlejem",
+                "Wśród nocnej ciszy",
+                "Jezus malusieńki",
+                "Wesoła nowina",
+                "Lulajże, Jezuniu (kołysanka)",
+                "Hej, w dzień narodzenia"
+            };
+        }
         public void OnGet()
         {
         }
