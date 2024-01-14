097341d5f217299c132a0c0ff7bf31a705543826
K4P1_090 <k4p1.090@gmail.com>
2024-01-14 19:07:39 +0100

    qweq

--- strona-layout/Program.cs
+++ strona-layout/Program.cs
@@ -1,10 +1,13 @@
+using Microsoft.Extensions.FileProviders;
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorPages();
+builder.Services.AddControllersWithViews();
+builder.Services.AddDirectoryBrowser();
 var app = builder.Build();
 app.UseStaticFiles();
 app.MapRazorPages();
 app.UseDefaultFiles();
-
+app.UseFileServer(enableDirectoryBrowsing: true);
 // app.MapGet("/", () => "Hello World!");
 
 app.Run();
