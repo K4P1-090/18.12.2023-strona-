ec1dd2537a3c7994893687a75d0013d31d9b326c
K4P1_090 <k4p1.090@gmail.com>
2024-01-14 11:28:55 +0100

    9876t9gyu

--- strona-layout/Pages/Page3.cshtml.cs
+++ strona-layout/Pages/Page3.cshtml.cs
@@ -1,10 +1,27 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Collections.Generic;
 
 namespace MyApp.Namespace
 {
     public class Page3Model : PageModel
     {
+        [BindProperty]
+        public List<string> Zwyczaje { get; set; }
+
+        public Page3Model()
+        {
+            Zwyczaje = new List<string>
+            {
+                "Ości z karpia w portfelu",
+                "Ubieranie choinki",
+                "Sianko pod obrusem",
+                "12 potraw wigilijnych",
+                "Prezenty pod choinką"
+                
+            };
+        }
+
         public void OnGet()
         {
         }
